39dc935108a2bd4f7bb74a64cd7256b0b0c52501
wata00913 <175d8639@gmail.com>
2023-08-24 13:00:13 +0900

    メッセージ一覧画面の遷移時にログインチェック処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def after_sign_in_path_for(resource)
@@ -15,5 +16,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+
+  def require_sign_in
+    redirect_to new_user_session_path, alert: 'ログインしてください。' unless user_signed_in?
   end
 end
