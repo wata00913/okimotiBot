4f0a5c4084cfeede59da6f44328201e6481605a2
wata00913 <175d8639@gmail.com>
2023-08-25 12:50:24 +0900

    ChannelMemberモデルの関連付けを設定

--- app/models/channel_member.rb
+++ app/models/channel_member.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class ChannelMember < ApplicationRecord
+  belongs_to :slack_channel
+  belongs_to :slack_account
 end
--- app/models/slack_account.rb
+++ app/models/slack_account.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class SlackAccount < ApplicationRecord
+  has_many :channel_members, dependent: :destroy
+  has_many :channels, through: :channel_members, source: :slack_channel
+
   validates :account_id, presence: true, uniqueness: true
   validates :name, presence: true
 end
