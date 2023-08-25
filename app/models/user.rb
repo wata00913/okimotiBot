30230b431cadb2f7ff7bcbc9d6e6d555dbb94be0
wata00913 <175d8639@gmail.com>
2023-08-25 11:02:51 +0900

    メールアドレスの最大入力文字数のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 15 }
+  validates :email, length: { maximum: 254 }
 end
