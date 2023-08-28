9f863e6ae8cbb80f43d0c5d59ec1e7ff02eba9cd
wata00913 <175d8639@gmail.com>
2023-08-28 11:57:25 +0900

    Messageモデルの関連付けを設定

--- app/models/channel_member.rb
+++ app/models/channel_member.rb
@@ -4,6 +4,8 @@ class ChannelMember < ApplicationRecord
   belongs_to :slack_channel
   belongs_to :slack_account
 
+  has_many :messages, dependent: :destroy
+
   has_many :observed_members, dependent: :destroy
   has_many :users, through: :observed_members
 
--- app/models/observed_member.rb
+++ app/models/observed_member.rb
@@ -3,6 +3,7 @@
 class ObservedMember < ApplicationRecord
   belongs_to :user
   belongs_to :channel_member
+  has_many :messages, through: :channel_member
 
   validates :user_id, presence: true, uniqueness: { scope: :channel_member_id }
   validates :channel_member_id, presence: true
