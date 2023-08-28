9c1a7555379b86c757e70ad66f119015818b12b8
wata00913 <175d8639@gmail.com>
2023-08-28 09:13:31 +0900

    welcome画面とそのルーティングの削除。ルートパスをメッセージ一覧画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'messages', to: 'messages#index'
-  get 'welcome/index'
-  root to: 'welcome#index'
+  root to: 'messages#index'
   devise_for :users, only: %i[registrations sessions]
 end
