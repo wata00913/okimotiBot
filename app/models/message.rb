429460f06bef07e8d00cd0c2d1e3c370c05295e4
wata00913 <175d8639@gmail.com>
2023-08-28 12:32:35 +0900

    SentimentScoreモデルの関連付けを設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  has_one :sentiment_score, dependent: :destroy
+
   validates :channel_member_id, :slack_timestamp, :original_message,
             presence: true
 end
--- app/models/sentiment_score.rb
+++ app/models/sentiment_score.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SentimentScore < ApplicationRecord
+  belongs_to :message
+
   validates :message_id, :positive, :negative, :neutral, :mixed,
             presence: true
 end
