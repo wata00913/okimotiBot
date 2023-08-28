fac3d635491e683ea53c07052d644f491be203dd
wata00913 <175d8639@gmail.com>
2023-08-28 09:18:46 +0900

    ログアウト後のリダイレクト先をログイン画面に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     messages_path
   end
 
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
