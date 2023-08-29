75af174d164450d69ad58653d9bca40011be0326
wata00913 <175d8639@gmail.com>
2023-08-29 13:00:59 +0900

    N+1クエリ問題を解消

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     ids = current_user.observed_members.pluck(:channel_member_id)
-    @messages = Message.where(channel_member_id: ids).order(slack_timestamp: :desc)
+    @messages = Message.includes(:sentiment_score, channel_member: %i[slack_channel slack_account])
+                       .where(channel_member_id: ids).order(slack_timestamp: :desc)
   end
 end
