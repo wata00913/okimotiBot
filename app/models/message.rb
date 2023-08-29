13bac8e5d0b8053bce72a1ea69b94024a7e604c1
wata00913 <175d8639@gmail.com>
2023-08-29 10:37:18 +0900

    MessageモデルからSlackChannelとSlackAccountを参照できるように関連付けを設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  belongs_to :channel_member
   has_one :sentiment_score, dependent: :destroy
 
   validates :channel_member_id, :slack_timestamp, :original_message,
