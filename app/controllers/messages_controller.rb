cb3da471230c3c75372594240282636a5338212d
wata00913 <175d8639@gmail.com>
2023-08-29 09:40:09 +0900

    メッセージ一覧画面を作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,7 @@ class MessagesController < ApplicationController
   before_action :require_sign_in
 
   def index
+    ids = current_user.observed_members.pluck(:channel_member_id)
+    @messages = Message.where(channel_member_id: ids).order(slack_timestamp: :desc)
   end
 end
