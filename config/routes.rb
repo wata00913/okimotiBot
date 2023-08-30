2ffa35bb4a7170af2ce3bf8ecadcd5f6b4b37bf9
wata00913 <175d8639@gmail.com>
2023-08-30 17:09:14 +0900

    カスタマイズ用のRegistrationsControllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'messages', to: 'messages#index'
   root to: 'messages#index'
-  devise_for :users, only: %i[registrations sessions]
+  devise_for :users, only: %i[registrations sessions], controllers: { registrations: 'users/registrations' }
 end
