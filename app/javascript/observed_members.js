57596ff1f20b9dfda64cc120437495355b14b9e1
wata00913 <175d8639@gmail.com>
2023-09-07 17:03:59 +0900

    メッセージ表示処理の修正

--- app/javascript/observed_members.js
+++ app/javascript/observed_members.js
@@ -133,9 +133,9 @@ window.onClickObservedChannelButton = function onClickObservedChannelButton(pare
 function createNoticeOrAlertMessageView(message, isError) {
   let colorClass = ""
   if (isError) {
-    colorClass = "bg-green-100 border-green-400 text-green-700"
-  } else {
     colorClass = "bg-red-100 border-red-400 text-red-700"
+  } else {
+    colorClass = "bg-green-100 border-green-400 text-green-700"
   }
   return escapeHTML`
     <div class="border ${colorClass} px-4 py-3 rounded relative" role="alert">
