6e408d09e46d13dd0b10f84982acae4f4742bc0d
wata00913 <175d8639@gmail.com>
2023-09-07 10:01:42 +0900

    チェックボックスのレイアウトをON、OFF切替のスイッチに変更

--- app/javascript/observed_members.js
+++ app/javascript/observed_members.js
@@ -142,9 +142,18 @@ function createUserView(channelInfo, userInfo) {
     <div id="${getObservedChannelMemberElId(channelInfo, userInfo)}" class="flex mb-1 px-4">
       <p class="flex items-center">${userInfo.name}</p>
       <img src="${userInfo.image_url}" alt="アカウントアイコン" class="ml-4 w-[50px] h-[50px]">
+      <p class="flex items-center ml-6">監視対象</p>
       <label for="${userInfo.channel_member_id}" class="flex items-center ml-4">
-        <input type="checkbox" onclick="onClickObservedMemberCheckBox(event, '${channelInfo.id}', ${userInfo.channel_member_id})" id="${userInfo.channel_member_id}" ${checkedStr} />
-        監視対象
+        <input type="checkbox" 
+          onclick="onClickObservedMemberCheckBox(event, '${channelInfo.id}', ${userInfo.channel_member_id})" id="${userInfo.channel_member_id}" ${checkedStr}
+          class="peer sr-only"
+        />
+        <span
+          class="block w-[2em] cursor-pointer bg-gray-500 rounded-full 
+          p-[1px] after:block after:h-[1em] after:w-[1em] after:rounded-full 
+          after:bg-white after:transition peer-checked:bg-blue-500 
+          peer-checked:after:translate-x-[calc(100%-2px)]"
+        >
       </label>
     </div>
     `
