dd217994e5d8ee1ebad86a587ba53e4638d9ab63
wata00913 <175d8639@gmail.com>
2023-09-11 11:38:08 +0900

    感情の検索機能を追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,19 @@ class MessagesController < ApplicationController
   def index
     ids = current_user.observed_members.pluck(:channel_member_id)
     @messages = Message.includes(:sentiment_score, channel_member: %i[slack_channel slack_account])
-                       .where(channel_member_id: ids).order(slack_timestamp: :desc)
+                       .where(channel_member_id: ids)
+                       .order(slack_timestamp: :desc)
                        .page(params[:page])
+
+    set_search_message
+  end
+
+  private
+
+  def set_search_message
+    search_params = params[:message]
+    return unless search_params
+
+    @messages = @messages.public_send("best_#{search_params[:best_emotion]}") unless search_params[:best_emotion].blank?
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,17 @@ class Message < ApplicationRecord
 
   validates :channel_member_id, :slack_timestamp, :original_message,
             presence: true
+
+  scope :best_positive, lambda {
+                          joins(:sentiment_score)
+                            .where('sentiment_scores.positive > sentiment_scores.negative
+                                    and sentiment_scores.positive > sentiment_scores.neutral') }
+  scope :best_negative, lambda {
+                          joins(:sentiment_score)
+                            .where('sentiment_scores.negative > sentiment_scores.positive
+                                   and sentiment_scores.negative > sentiment_scores.neutral') }
+  scope :best_neutral, lambda {
+                         joins(:sentiment_score)
+                           .where('sentiment_scores.neutral > sentiment_scores.negative
+                                   and sentiment_scores.neutral > sentiment_scores.positive') }
 end
