7c1452fd29c7abb0142ac3421dc82b4ee28e5d99
wata00913 <175d8639@gmail.com>
2023-09-12 12:04:57 +0900

    不要なメソッドを削除

--- app/models/slack_client.rb
+++ app/models/slack_client.rb
@@ -5,17 +5,6 @@ class SlackClient
     @client = Slack::Web::Client.new
   end
 
-  def operate_slack_api
-    begin
-      @client.auth_test
-      yield @client
-      true
-    rescue Slack::Web::Api::Errors::NotAuthed => e
-      Rails.logger.error e.full_message.chomp
-      false
-    end
-  end
-
   def fetch_channels
     @client.conversations_list['channels']
   end
