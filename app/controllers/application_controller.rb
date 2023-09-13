0f876d4622d9621466d5693249ad394f1c703105
wata00913 <175d8639@gmail.com>
2023-09-13 11:30:00 +0900

    独自クラスのSlackClientをlibディレクトリに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'slack_client'
+
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
