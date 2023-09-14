26e9ffdca54cba7ca794bffa45843bd092902ce2
wata00913 <175d8639@gmail.com>
2023-09-14 15:51:54 +0900

    メソッド名が間違えていたので修正

--- app/controllers/api/slack_channels_controller.rb
+++ app/controllers/api/slack_channels_controller.rb
@@ -7,7 +7,7 @@ class Api::SlackChannelsController < Api::ApplicationController
     channels_response = slack_client.fetch_channels
 
     channels_response.each do |channel_response|
-      SlackChannel.find_or_create_by_attr(channel_response, channel_id_key: :id)
+      SlackChannel.find_or_create_by_attr!(channel_response, channel_id_key: :id)
     end
     @updated_at = Time.zone.now
   end
